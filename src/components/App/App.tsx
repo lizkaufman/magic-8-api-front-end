72e89b9a3651e2da1427afa5c80626b71507389a
Liz Kaufman <lizkaufman92@gmail.com>
2023-01-10 15:09:29 +0000

    basic MVP done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
 import "./App.css";
+import GitHubButton from "react-github-btn";
 import Ball from "../Ball";
 import Answer from "../Answer";
 
 function App() {
-  const [answer, setAnswer] = useState("Awaiting the sages...");
+  const [answer, setAnswer] = useState("...");
 
   async function fetchAnswer(): Promise<void> {
-    console.log("working");
+    const res = await fetch("https://magic-8-api.onrender.com/");
+    const data = await res.json();
+    setAnswer(data.answer);
   }
 
   return (
-    <main>
-      <h1>Magic 8 Ball</h1>
-      <h2>
-        A free API that brings the prognosticating power of the magic 8 ball
-        into the 21st century
-      </h2>
-      <a href="https://magic-8-api.onrender.com/api">
-        <h3>https://magic-8-api.onrender.com/api</h3>
-      </a>
-      <Ball handleClick={fetchAnswer} />
-      <p className="regular-text">Click the ball above to try it out.</p>
-      <Answer answer={answer} />
-    </main>
+    <>
+      <main>
+        <h1>Magic 8 Ball</h1>
+        <h2>
+          A free API that brings the prognosticating power of the magic 8 ball
+          into the 21st century
+        </h2>
+        <a href="https://magic-8-api.onrender.com/">
+          <h3>https://magic-8-api.onrender.com/</h3>
+        </a>
+        <Ball handleClick={fetchAnswer} />
+        <p className="regular-text">Click the ball above to try it out.</p>
+        <Answer answer={answer} />
+      </main>
+      <footer>
+        <GitHubButton
+          href="https://github.com/lizkaufman/magic-8-api"
+          data-color-scheme="no-preference: light_high_contrast; light: light_high_contrast; dark: dark_high_contrast;"
+          data-size="large"
+          aria-label="Star lizkaufman/magic-8-api on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </footer>
+    </>
   );
 }
 
